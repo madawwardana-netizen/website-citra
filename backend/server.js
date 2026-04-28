ca08ce8b3885c67cbfd3c1bffb54ad1572c62ca8
afterhoursfeb2date5-code <afterhoursfeb2date5@gmail.com>
2026-04-28 15:40:06 +0700

    update project website citra

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 // Import routes
@@ -28,6 +29,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files dari folder frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Request logging middleware
 app.use((req, res, next) => {
